cb4f2e6ecf1388fd27a29c1a34702f997534a3b1
skorzany <177423232+skorzany@users.noreply.github.com>
2025-02-05 18:00:43 +0100

    Refactor & finish the logic for playing in console

--- script.js
+++ script.js
@@ -1,69 +1,70 @@
 function cell(symbol="") {
-    let content = symbol;
+    let marker = symbol;
 
-    const getContent = () => content;
-    const setContent = (c) => content = c;
-    const isEmpty = () => !content;
+    const getMarker = () => marker;
+    const setMarker = (m) => marker = m;
+    const isEmpty = () => !marker;
 
     return {
-        getContent,
-        setContent,
+        getMarker,
+        setMarker,
         isEmpty,
     }
 }
 
-function gameBoard(rows, columns) {
-    const rowNum = rows;
-    const colNum = columns;
+function gameBoard() {
+    const ROWNUM = 3;
+    const COLNUM = 3;
     const board = [];
 
-    for(r = 0; r < rowNum; r++) {
+    for(r = 0; r < ROWNUM; r++) {
         const row = [];
-        for(c = 0; c < colNum; c++) {
+        for(c = 0; c < COLNUM; c++) {
             row.push(cell());
         }
         board.push(row);
     }
 
-    const placeMarker = function (cell, player) {
+    const placeMarker = function (cell, marker) {
         if (cell.isEmpty()){
-            cell.setContent(player.getMarker());
+            cell.setMarker(marker);
         }
     }
 
     const getBoard = () => board;
 
-    const winningState = function (symbol) {
-        const matchingSymbol = (obj) => obj.getContent() === symbol;
+    const findTriple = function (symbol) {
+        let triples = [];
+        const matchingSymbol = (obj) => obj.getMarker() === symbol;
         // rows
         for(row of board) {
-            if(row.every(matchingSymbol)) return true;
+            if(row.every(matchingSymbol)) triples.push(row);
         }
         // columns
-        const columns = [
-            [board[0][0], board[1][0], board[2][0]],
-            [board[0][1], board[1][1], board[2][1]],
-            [board[0][2], board[1][2], board[2][2]],
-        ];
-        for(col of columns) {
-            if (col.every(matchingSymbol)) return true;
+        for(let r = 0; r < ROWNUM; r++) {
+            const column = [];
+            for(let c = 0; c < COLNUM; c++) {
+                column.push(board[c][r]);
+            }
+            if(column.every(matchingSymbol)) triples.push(column);
         }
         // diagonals
-        const diagonals = [
-            [board[0][0], board[1][1], board[2][2]],
-            [board[2][0], board[1][1], board[0][2]],
-        ];
-        for(diag of diagonals) {
-            if (diag.every(matchingSymbol)) return true;
+        const diagonals = [[], []];
+        for(let i = 0; i < ROWNUM; i++) {
+            diagonals[0].push(board[i][i]);
+            diagonals[1].push(board[i][-i + 2]);
         }
-        return false;
+        for(diagonal of diagonals){
+            if(diagonal.every(matchingSymbol)) triples.push(diagonal);
+        }
+
+        return triples;
     }
 
     //below are helper functions only for console version, they will be removed later
 
     const printBoard = () => {
-        console.log("printing board");
-        const output = board.map((row => (row.map((cell) => cell.getContent()))));
+        const output = board.map((row => (row.map((cell) => cell.getMarker()))));
         console.log(output);
     }
 
@@ -72,7 +73,7 @@ function gameBoard(rows, columns) {
     return {
         placeMarker,
         getBoard,
-        winningState,
+        findTriple,
         printBoard,
         getCell,
     }
@@ -92,122 +93,84 @@ function player(name, marker) {
 }
 
 function ticTacToe() {
-    const player1 = player("John", "X");
-    const player2 = player("Jenny", "O");
-    const players = [player1, player2];
-    const board = gameBoard(3, 3);
+    const players = [player("John", "X"), player("Jenny", "O")];
+    let roundNumber;
+    let board;
+    let gameEnded;
+    let currentPlayer;
+    let tilesToHighlight;
 
-    let roundNumber = 0;
-
+    const endGame = () => gameEnded = true;
     const advanceRound = () => roundNumber++;
-    const getActivePlayer = () => players[roundNumber%2];
-    const showCurrentRound = () => {
-        board.printBoard();
-        console.log(`Round ${roundNumber + 1}: ${getActivePlayer().getName()}'s turn`);
+    const getCurrentPlayer = () => players[roundNumber%2];
+    const showInfo = () => {
+        console.log(`It's ${currentPlayer.getMarker()} (${currentPlayer.getName()}'s) turn.`)
     }
     const grabInput = () => {
-        const r = prompt("Row number");
-        const c = prompt("Column number");
-        return [r, c];
+        let emptyCell = false;
+        while(!emptyCell) {
+            var row = prompt("Row number?");
+            var column = prompt("Column number?");
+            emptyCell = board.getCell(row, column).isEmpty();
+            if(!emptyCell) alert("This cell is already claimed!");
+        }
+        return [row, column];
     }
-    const checkForWinner = () => {
-        const currentPlayerMarker = getActivePlayer().getMarker();
-        return board.winningState(currentPlayerMarker);
+    const viewSummary = (winningCells, player) => {
+        let msg = "Game ended. "
+        if(winningCells.length) {
+            msg += `${player.getMarker()} (${player.getName()}) wins!`;
+        }
+        else {
+            msg += "It's a tie!";
+        }
+        console.log(msg);
+        board.printBoard();
+        console.log(winningCells);
     }
     const playRound = () => {
-        showCurrentRound();
-        const where = grabInput();
-        const cell = board.getCell(...where);
-        board.placeMarker(cell, getActivePlayer());
-        checkForWinner() ? console.log(`${getActivePlayer().getName()} is a winner`):console.log('game on');
+        // change current player;
+        currentPlayer = getCurrentPlayer();
+        // grab current player's symbol;
+        const symbol = currentPlayer.getMarker();
+        // show info & the board;
+        showInfo();
+        board.printBoard();
+        // gather input (it should ask forever until it is valid);
+        const coords = grabInput();
+        // place marker on given cell;
+        board.placeMarker(board.getCell(...coords), symbol);
+        // check for win condition but only when there were at least 4 rounds played;
+        if(4 <= roundNumber) {
+            const winningCells = board.findTriple(symbol);
+            if(winningCells.length || roundNumber === 8) {
+                endGame();
+                return winningCells;
+            }
+        }
         advanceRound();
-        showCurrentRound();
+    }
+    const setDefaultState = () => {
+        roundNumber = 0;
+        board = gameBoard();
+        gameEnded = false;
+        tilesToHighlight = [];
+    }
+    const newGame = () => {
+        setDefaultState();
+        while(!gameEnded) {
+            roundOutput = playRound();
+            if(roundOutput !== undefined) tilesToHighlight.push(...roundOutput);
+        }
+        viewSummary(tilesToHighlight, currentPlayer);
     }
 
     return {
-        playRound,
+        newGame,
     }
 }
 
-// CELL TESTS
-// const empty = cell();
-// const x = cell("X");
-
-// console.log(empty.isEmpty());
-// console.log(x.isEmpty());
-// console.log(x.getContent());
-// empty.setContent("O");
-// console.log(empty.isEmpty());
-// console.log(empty.getContent());
-
-// BOARD TESTS
-// const board = gameBoard(3, 3);
-// console.log(board.getBoard());
-// console.log(board.printBoard());
-    // scan rows
-// console.log(`all empty: ${board.scanRows("")}`);
-// c = board.getCell(1, 0);
-// d = board.getCell(1, 1);
-// e = board.getCell(1, 2);
-// f = board.getCell(2, 0);
-// g = board.getCell(2, 1);
-// h = board.getCell(2, 2);
-// board.placeMarker(c, player("john", "X"));
-// board.placeMarker(d, player("john", "X"));
-// board.placeMarker(e, player("john", "X"));
-// console.log(`all 'x': ${board.scanRows("X")}`);
-// board.placeMarker(f, player("jenny", "O"));
-// board.placeMarker(g, player("jenny", "O"));
-// board.placeMarker(h, player("jenny", "O"));
-// console.log(`all 'o': ${board.scanRows("O")}`);
-// console.log(board.printBoard());
-// console.log(board.getCell(1, 1).getContent());
-    // scan cols
-// console.log(`all empty: ${board.scanColumns("")}`);
-// c = board.getCell(0, 1);
-// d = board.getCell(1, 1);
-// e = board.getCell(2, 1);
-// f = board.getCell(0, 2);
-// g = board.getCell(1, 2);
-// h = board.getCell(2, 2);
-// board.placeMarker(c, player("john", "X"));
-// board.placeMarker(d, player("john", "X"));
-// board.placeMarker(e, player("john", "X"));
-// console.log(`all 'x': ${board.scanColumns("X")}`);
-// board.placeMarker(f, player("jenny", "O"));
-// board.placeMarker(g, player("jenny", "O"));
-// board.placeMarker(h, player("jenny", "O"));
-// console.log(`all 'o': ${board.scanColumns("O")}`);
-// console.log(board.printBoard());
-// console.log(board.getCell(1, 1).getContent());
-    // scan diagonals
-// console.log(`all empty: ${board.scanDiagonals("")}`);
-// c = board.getCell(0, 0);
-// d = board.getCell(1, 1);
-// e = board.getCell(2, 2);
-// f = board.getCell(2, 0);
-// g = board.getCell(1, 1);
-// h = board.getCell(0, 2);
-// board.placeMarker(c, player("john", "X"));
-// board.placeMarker(d, player("john", "X"));
-// board.placeMarker(e, player("john", "X"));
-// console.log(`all 'x': ${board.scanDiagonals("X")}`);
-// board.placeMarker(f, player("jenny", "O"));
-// board.placeMarker(g, player("jenny", "O"));
-// board.placeMarker(h, player("jenny", "O"));
-// console.log(`all 'o': ${board.scanDiagonals("O")}`);
-// console.log(board.printBoard());
-// console.log(board.getCell(1, 1).getContent());
-
-// PLAYER TESTS
-// const p1 = player("Michael", "X");
-// const p2 = player("Jenny", "O");
-
-// console.log(`Player 1 name: ${p1.getName()}`);
-// console.log(`Player 2 marker: '${p2.getMarker()}'`);
 
 // WHOLE GAME (9 ROUNDS)
 const game = ticTacToe();
-for(let i = 0; i < 9; i++){
-    game.playRound()
-}
+game.newGame();
