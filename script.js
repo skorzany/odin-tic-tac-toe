0eaadcb06c8aae64050640b7c51cb06e2e419132
skorzany <177423232+skorzany@users.noreply.github.com>
2025-02-12 20:00:45 +0100

    Finish DOM/logic separation & cleanup the code

--- script.js
+++ script.js
@@ -1,136 +1,21 @@
 const ticTacToe = (function () {
     const ROUNDLIMIT = 8;
+    const MINWINROUND = 4;
 
     let roundNumber;
     let currentPlayer;
-    let tilesToHighlight;
     let phase;
     let players = [undefined, undefined];
 
-    const [header, main, _] = document.querySelectorAll("body > *");
+    const [header, main] = document.querySelectorAll("body > *");
     const button = header.querySelector(".content .header-top button");
     const formContainer = header.querySelector(".content .header-bot");
     const form = formContainer.querySelector("form");
     const inputs = [...form.querySelectorAll("input[type='text']")];
     const boardContainer = main.querySelector(".content .board");
     const info = main.querySelector(".content .info-box p");
-    const board = (function () {
-        const ROWNUM = 3;
-        const COLNUM = 3;
-        const board = [];
-        function cell(symbol="") {
-            let marker = symbol;
+    const tilesToHighlight = new Set();
 
-            const getMarker = () => marker;
-            const setMarker = (m) => marker = m;
-            const isEmpty = () => !marker;
-
-            return {
-                getMarker,
-                setMarker,
-                isEmpty,
-            }
-        };
-
-        for(let r = 0; r < ROWNUM; r++) {
-            const row = [];
-            for(let c = 0; c < COLNUM; c++) {
-                row.push(cell());
-            }
-            board.push(row);
-        }
-
-        const getBoard = () => board;
-        const clear = () => {
-            for(let row of board) {
-                for(let c = 0; c < COLNUM; c++) {
-                    row[c] = cell();
-                }
-            }
-        }
-        const placeMarker = function (cell, marker) {
-            if(cell.isEmpty()){
-                cell.setMarker(marker);
-            }
-        }
-        const findTriples = function (symbol) {
-            let triples = new Set();
-            const matchingSymbol = (obj) => obj.getMarker() === symbol;
-            // rows
-            for(let row of board) {
-                if(row.every(matchingSymbol)) row.forEach(item => triples.add(item.i));
-            }
-            // columns
-            for(let r = 0; r < ROWNUM; r++) {
-                const column = [];
-                for(let c = 0; c < COLNUM; c++) {
-                    column.push(board[c][r]);
-                }
-                if(column.every(matchingSymbol)) column.forEach(item => triples.add(item.i));
-            }
-            // diagonals
-            const diagonals = [[], []];
-            for(let i = 0; i < ROWNUM; i++) {
-                diagonals[0].push(board[i][i]);
-                diagonals[1].push(board[i][-i + 2]);
-            }
-            for(let diagonal of diagonals){
-                if(diagonal.every(matchingSymbol)) diagonal.forEach(item => triples.add(item.i));
-            }
-            return triples;
-        }
-
-        return {
-            getBoard,
-            clear,
-            placeMarker,
-            findTriples,
-        }
-    })();
-    const viewer = (function () {
-        const createBoardView = () => {
-            let idx = 0;
-            for(let row of board.getBoard()) {
-                for(let obj of row) {
-                    obj.i = idx;
-                    const newCell = document.createElement("div");
-                    newCell.setAttribute("data-idx", idx);
-                    newCell.classList.add("cell", "empty");
-                    newCell.object = obj;
-                    boardContainer.appendChild(newCell);
-                    idx += 1;
-                }
-            }
-        }
-        const deleteBoardView = () => boardContainer.replaceChildren();
-        const showForm = () => formContainer.classList.remove("hidden");
-        const hideForm = () => formContainer.classList.add("hidden");
-        const clearInputs = () => {
-            for(let ele of inputs) ele.value = "";
-        }
-        const showButton = () => button.classList.remove("invisible");
-        const hideButton = () => button.classList.add("invisible");
-        const updateBoard = () => {
-            for(let ele of boardContainer.children) {
-                const obj = ele.object;
-                ele.textContent = obj.getMarker();
-                if(!obj.isEmpty()) ele.classList.remove("empty");
-            }
-        }
-        const updateInfo = (text) => info.textContent = text;
-
-        return {
-            createBoardView,
-            deleteBoardView,
-            showForm,
-            hideForm,
-            clearInputs,
-            showButton,
-            hideButton,
-            updateBoard,
-            updateInfo,
-        }
-    })();
     const initialize = () => {
         phase = "start-new";
         viewer.updateInfo(createMessage());
@@ -149,6 +34,20 @@ const ticTacToe = (function () {
             newGame();
         });
     }
+    const newGame = () => {
+        phase = "game";
+        setDefaultState();
+        createPlayers();
+        setCurrentPlayer();
+        viewer.createBoardView();
+        viewer.updateInfo(createMessage());
+        boardContainer.addEventListener("click", playRound);
+    }
+    const setDefaultState = () => {
+        roundNumber = 0;
+        board.clear();
+        tilesToHighlight.clear();
+    }
     const createPlayers = () => {
         function player(name, marker) {
             const myName = name;
@@ -166,32 +65,17 @@ const ticTacToe = (function () {
         players[0] = player(p1name, "X");
         players[1] = player(p2name, "O");
     }
-    const advanceRound = () => roundNumber++;
     const setCurrentPlayer = () => currentPlayer = players[roundNumber%2];
-    const createMessage = () => {
-        switch (phase) {
-            case "start-new": return "Greetings! Click 'Start new game' to play!";
-            case "start-inputs": return "Waiting for player names...";
-            case "game": return `It's ${currentPlayer.getName()}'s (${currentPlayer.getMarker()}) turn.`;
-            case "end-tie": return "Game end. It's a tie!";
-            case "end-winner": return `Game end. ${currentPlayer.getName()} (${currentPlayer.getMarker()}) wins!`;
-        }
-    }
     const playRound = (event) => {
         const ele = event.target;
         const symbol = currentPlayer.getMarker();
-        if(ele.classList.contains("empty")) {
-            ele.classList.remove("empty");
+        if(ele.object.isEmpty()) {
             ele.object.setMarker(symbol);
             viewer.updateBoard();
-            if(0.5*ROUNDLIMIT <= roundNumber) tilesToHighlight = board.findTriples(symbol);
-            if(tilesToHighlight.size) phase = "end-winner";
-            else if(roundNumber === ROUNDLIMIT) phase = "end-tie";
+            if(MINWINROUND <= roundNumber) board.findTriples(symbol);
+            phase = tilesToHighlight.size ? "end-winner" : ((roundNumber === ROUNDLIMIT) ? "end-tie" : phase);
             if(phase.includes("end")) {
-                for(let child of boardContainer.children) {
-                    child.classList.remove("empty");
-                    if(tilesToHighlight.has(Number(child.dataset.idx))) child.classList.add("winner");
-                }
+                viewer.updateBoard();
                 viewer.updateInfo(createMessage());
                 boardContainer.removeEventListener("click", playRound);
                 return;
@@ -201,20 +85,129 @@ const ticTacToe = (function () {
             viewer.updateInfo(createMessage());
         }
     }
-    const setDefaultState = () => {
-        roundNumber = 0;
-        board.clear();
-        tilesToHighlight = [];
+    const createMessage = () => {
+        switch (phase) {
+            case "start-new": return "Greetings! Click 'Start new game' to play!";
+            case "start-inputs": return "Waiting for player names...";
+            case "game": return `It's ${currentPlayer.getName()}'s (${currentPlayer.getMarker()}) turn.`;
+            case "end-tie": return "Game end. It's a tie!";
+            case "end-winner": return `Game end. ${currentPlayer.getName()} (${currentPlayer.getMarker()}) wins!`;
+        }
     }
-    const newGame = () => {
-        phase = "game";
-        setDefaultState();
-        createPlayers();
-        setCurrentPlayer();
-        viewer.createBoardView();
-        viewer.updateInfo(createMessage());
-        boardContainer.addEventListener("click", playRound);
-    }
+    const advanceRound = () => roundNumber++;
+    const board = (function () {
+        const ROWNUM = 3;
+        const COLNUM = 3;
+        const board = [];
+        function cell() {
+            let marker = "";
+
+            const getMarker = () => marker;
+            const setMarker = (m) => marker = m;
+            const isEmpty = () => !marker;
+
+            return {
+                getMarker,
+                setMarker,
+                isEmpty,
+            }
+        };
+        for(let r = 0; r < ROWNUM; r++) {
+            const row = [];
+            for(let c = 0; c < COLNUM; c++) {
+                row.push(cell());
+            }
+            board.push(row);
+        }
+        const getBoard = () => board;
+        const clear = () => {
+            for(let row of board) {
+                for(let c = 0; c < COLNUM; c++) {
+                    row[c] = cell();
+                }
+            }
+        }
+        const placeMarker = function (cell, marker) {
+            if(cell.isEmpty()) cell.setMarker(marker);
+        }
+        const findTriples = function (symbol) {
+            const matchingSymbol = (obj) => obj.getMarker() === symbol;
+            // rows
+            for(let row of board) {
+                if(row.every(matchingSymbol)) row.forEach(item => tilesToHighlight.add(item.i));
+            }
+            // columns
+            for(let r = 0; r < ROWNUM; r++) {
+                const column = [];
+                for(let c = 0; c < COLNUM; c++) {
+                    column.push(board[c][r]);
+                }
+                if(column.every(matchingSymbol)) column.forEach(item => tilesToHighlight.add(item.i));
+            }
+            // diagonals
+            const diagonals = [[], []];
+            for(let i = 0; i < ROWNUM; i++) {
+                diagonals[0].push(board[i][i]);
+                diagonals[1].push(board[i][-i + 2]);
+            }
+            for(let diagonal of diagonals){
+                if(diagonal.every(matchingSymbol)) diagonal.forEach(item => tilesToHighlight.add(item.i));
+            }
+        }
+
+        return {
+            getBoard,
+            clear,
+            placeMarker,
+            findTriples,
+        }
+    })();
+    const viewer = (function () {
+        const deleteBoardView = () => boardContainer.replaceChildren();
+        const showForm = () => formContainer.classList.remove("hidden");
+        const hideForm = () => formContainer.classList.add("hidden");
+        const clearInputs = () => inputs.forEach((ele) => ele.value = "");
+        const showButton = () => button.classList.remove("invisible");
+        const hideButton = () => button.classList.add("invisible");
+        const updateInfo = (text) => info.textContent = text;
+        const createBoardView = () => {
+            let idx = 0;
+            for(let row of board.getBoard()) {
+                for(let obj of row) {
+                    obj.i = idx;
+                    const newCell = document.createElement("div");
+                    newCell.classList.add("cell", "empty");
+                    newCell.object = obj;
+                    boardContainer.appendChild(newCell);
+                    idx += 1;
+                }
+            }
+        }
+        const updateBoard = () => {
+            const triplesFound = !!tilesToHighlight.size;
+            for(let ele of boardContainer.children) {
+                const obj = ele.object;
+                ele.textContent = obj.getMarker();
+                if(triplesFound) {
+                    ele.classList.remove("empty");
+                    if(tilesToHighlight.has(obj.i)) ele.classList.add("winner");
+                }
+                else if(!obj.isEmpty()) ele.classList.remove("empty");
+            }
+        }
+
+        return {
+            deleteBoardView,
+            showForm,
+            hideForm,
+            clearInputs,
+            showButton,
+            hideButton,
+            updateInfo,
+            createBoardView,
+            updateBoard,
+        }
+    })();
 
     return {
         initialize,
